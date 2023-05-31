afc57abe6b9c346e466155b878de2967630458f0
SaipavanMaddala <sai984942@gmail.com>
2023-05-31 15:52:06 +0530

    siva sir code

--- src/com/siva/AdditionOfTwoNumbers.java
+++ src/com/siva/AdditionOfTwoNumbers.java
@@ -4,9 +4,11 @@ public class AdditionOfTwoNumbers {
 
 	public static void main(String[] args) {
 
-		int num1 = 10;
-		int num2 = 20;
-		int result = num1 + num2;
+		int num1 = 70;
+		int num2 = 80;
+		int num3=60;
+
+		int result = num1 + num2+num3;
 		System.out.println("Addition of Two Numbers  " + result);
 
 	}
