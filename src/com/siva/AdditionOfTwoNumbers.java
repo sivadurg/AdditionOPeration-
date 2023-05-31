ddc7ca9b90df512652461053dc07befde306a417
siva <sivadurgarao@gmail.com>
2023-05-31 14:58:27 +0530

    code

---
+++ src/com/siva/AdditionOfTwoNumbers.java
@@ -0,0 +1,14 @@
+package com.siva;
+
+public class AdditionOfTwoNumbers {
+
+	public static void main(String[] args) {
+
+		int num1 = 10;
+		int num2 = 20;
+		int result = num1 + num2;
+		System.out.println("Addition of Two Numbers  " + result);
+
+	}
+
+}
